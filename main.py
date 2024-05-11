36f738c6edf70ca60fc11c0c13e7179ce37a955a
Efekan Tosmak <tsmkefekan05@outlook.com>
2024-05-12 01:59:57 +0300

    Updated web scraping script to extract company names and program details

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get('https://www.youthall.com/tr/jobs/?page=1').text
+soup = BeautifulSoup(html_text, 'lxml')
+adverts = soup.find_all('div', class_ = 'l-grid__col l-grid__col--lg-4 l-grid__col--md-4 l-grid__col--xs-12 u-gap-bottom-25')
+for advert in adverts:
+    company = advert.find('small', class_ = 'u-clear-gap u-text-overflow').text
+    program = advert.find('h5', class_ = 'u-text-overflow').text
+    print(company, program, end="\n\n")
+
+
 
