e0c30b6e0e4336a20859a9ac06746155eddd2d8b
Efekan Tosmak <tsmkefekan05@outlook.com>
2024-05-14 00:00:21 +0300

    Added filtering feature and implemented scraping of all pages in the web scraper

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://www.youthall.com/tr/jobs/?page=1').text
-soup = BeautifulSoup(html_text, 'lxml')
-adverts = soup.find_all('div', class_ = 'l-grid__col l-grid__col--lg-4 l-grid__col--md-4 l-grid__col--xs-12 u-gap-bottom-25')
-for advert in adverts:
-    company = advert.find('small', class_ = 'u-clear-gap u-text-overflow').text
-    program = advert.find('h5', class_ = 'u-text-overflow').text
-    print(company, program, end="\n\n")
+page = 1
+while True:
+    url = f'https://www.youthall.com/tr/jobs/?page={page}'
+    html_text = requests.get(url).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    adverts = soup.find_all('div', class_='l-grid__col'
+                                          ' l-grid__col--lg-4 l-grid__col--md-4 '
+                                          'l-grid__col--xs-12 u-gap-bottom-25')
+    if not adverts:
+        break
 
-
-
+    for advert in adverts:
+        company = advert.find('small', class_='u-clear-gap u-text-overflow').text
+        program = advert.find('h5', class_='u-text-overflow').text
+        lower_program = advert.find('h5', class_='u-text-overflow').text.lower()
+        if 'python' in lower_program or 'data' in lower_program:
+            print(f'''
+            Company Name: {company}
+            Program: {program}
+            ''')
+    page += 1
